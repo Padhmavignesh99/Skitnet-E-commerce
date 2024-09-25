65d2c3a1b3c190144564bc14fbc78d0b83735d6d
padhmavignesh <rpadhmavignesh@gmail.com>
2024-09-25 12:59:45 +0530

    End of Section 8

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+          id : number;
+          name : number;
+          description : string;
+          price : string;
+          pictureUrl : string;
+          type : string;
+          brand : string;
+          quantityInStock : number;
+}
