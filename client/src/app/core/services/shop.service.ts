dd8a870986b831cb4428ea52cff0a491c903097f
padhmavignesh <rpadhmavignesh@gmail.com>
2024-10-01 16:32:47 +0530

    End of Section 10

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -55,4 +55,8 @@ export class ShopService {
       next: response => this.types = response
     });
   }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
 }
