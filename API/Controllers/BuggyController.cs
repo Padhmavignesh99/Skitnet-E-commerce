febbf4d424df9a3966d2cd731ca3b30a5b221eb9
padhmavignesh <rpadhmavignesh@gmail.com>
2024-10-09 17:56:09 +0530

    End of Section 14

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,7 @@
+using System.Security.Claims;
 using API.Dtos;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -31,6 +33,14 @@ namespace API.Controllers
         public IActionResult GetValidationError(CreateProductDto product)
         {
             return Ok();
+        }
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            var name = User.FindFirst(ClaimTypes.Name)?.Value;
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Ok("Hello " + name + " with the id of " + id);
         }
     }
 }
